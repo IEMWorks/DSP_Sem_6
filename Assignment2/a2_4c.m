0b6df5eb6ff9012983f511a8417f0dce5c39f1c1
Subhajitdas298 <subhajitdas298@live.com>
2018-05-05 14:03:28 +0530

    updated a2q4 and a3q7

--- Assignment2/a2_4c.m
+++ Assignment2/a2_4c.m
@@ -1,12 +1,26 @@
 clc 
 clear all
 close all
+
 syms z n;
 
-x=z/z-5;
-y=iztrans(x)
-simplify(y)
-y2=ztrans(n*y)
-simplify(y2)
-y3=-z*diff(z/(z-5))
-simplify(y3)
+a = 5;
+xz = z/(z-a)
+xn = iztrans(xz);
+xn = simplify(xn)
+
+% for a
+disp('For equation a :')
+lhs = simplify(ztrans((a^n)*xn))
+rhs = simplify(subs(xz, z, (z/a)))
+
+% for b
+disp('For equation b :')
+n0 = 2
+lhs = simplify(ztrans(subs(xn, n, (n-n0))))
+rhs = simplify((z^(-n0))*xz)
+
+% for c
+disp('For equation c :')
+lhs = simplify(ztrans(n*xn))
+rhs = simplify(-z*diff(xz))
--- Assignment3/a3q7.m
+++ Assignment3/a3q7.m
@@ -12,7 +12,7 @@ ns=0:N-1;
 y=zeros(1,N);
 for n=1:N
     for k=1:N
-        y(n)=y(n)+x(k)*exp(-1i*2*pi*(n-1)*(k-1)/N);
+        y(n)=y(n)+x(k)*exp(1i*2*pi*(n-1)*(k-1)/N);
     end
     y(n)=y(n)/N;
 end
